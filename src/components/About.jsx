352ae680bddbffa802a78223d3c3b39445c3c96b
Sridhar <sridhar_maskeri@persistent.com>
2022-12-31 11:48:34 +0530

    Updated About.css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,19 +3,19 @@ import "./About.css";
 import profilePic from "../assets/profilePic.png";
 
 const About = () => {
-	return (
-		<div className="about">
-				<div className="clip-path-poly-top"/>
-			<div>
-				<img className="profile-pic" src={profilePic} alt="Profile Pic" />
-			</div>
-			<div className="about-intro">
-				<div className="intro-titles">
-					<p id="title-1">My Intro ✨</p>
-					<h1 id="title-name">About Me ✌️</h1>
-					<p>Curious, sincere and hardworking: These 3 things define me.</p>
-				</div>
-				{/* <div className="education">
+  return (
+    <div className="about">
+      <div />
+      <div>
+        <img className="profile-pic" src={profilePic} alt="Profile Pic" />
+      </div>
+      <div className="about-intro">
+        <div className="intro-titles">
+          <p id="title-1">My Intro ✨</p>
+          <h1 id="title-name">About Me ✌️</h1>
+          <p>Curious, sincere and hardworking: These 3 things define me.</p>
+        </div>
+        {/* <div className="education">
 					<p className="interests-title">Education 📖</p>
 					<div>
 						<h4>Goa College of Engineering</h4>
@@ -26,19 +26,19 @@ const About = () => {
 						<p>Grade : 83.91%</p>
 					</div>
 				</div> */}
-				<div className="interests">
-					<p className="interests-title">My Interests 😍</p>
-					<div className="interests-list">
-						<p>Drawing 🎨</p>
-						<p>Cycling 🚴</p>
-						<p>Coding 👨‍💻</p>
-						<p>Music 🎧</p>
-						<p>Running 🏃</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+        <div className="interests">
+          <p className="interests-title">My Interests 😍</p>
+          <div className="interests-list">
+            <p>Drawing 🎨</p>
+            <p>Cycling 🚴</p>
+            <p>Coding 👨‍💻</p>
+            <p>Music 🎧</p>
+            <p>Running 🏃</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;

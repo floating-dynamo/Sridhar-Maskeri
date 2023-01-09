f10512633f9eec63fdd54e15fcafac51b4b405e8
floating-dynamo <shridharmaskeri@gmail.com>
2023-01-09 22:29:02 +0530

    Updated the Blog Section

--- src/components/blog/BlogList.jsx
+++ src/components/blog/BlogList.jsx
@@ -4,6 +4,7 @@ import axios from "../../api";
 import "./BlogList.css";
 import { useNavigate } from "react-router-dom";
 import { generateSlug } from "../../utils";
+import ReactHtmlParser from "react-html-parser";
 
 const BlogList = () => {
   const [blogData, setBlogData] = useState({ status: "", feed: {}, items: [] });
@@ -32,21 +33,17 @@ const BlogList = () => {
       <div className="bloglist-blogs">
         {blogData?.items.map((blog) => {
           return (
-            <div
-              key={blog.title}
-              className="bloglist-blog"
-              onClick={() => {
-                const slug = generateSlug(blog.title);
-                navigate(`/blog/${slug}`);
-              }}
-            >
-              <div className="blog-thumbnail">
+            <div key={blog.title} className="bloglist-blog">
                 <img src={blog.thumbnail} alt={blog.title} />
-                <p className="pub-date">
-                  Published on: {blog.pubDate.split(" ")[0]}
-                </p>
+              <p className="pub-date">
+                Published on: {blog.pubDate.split(" ")[0]}
+              </p>
+              <div className="blog-content">
+                <p className="title">{blog.title}</p>
+                <a className="btn blog-read-more-btn" target="_blank" rel="noopener noreferrer" href={blog.link}>
+                  Read Blog
+                </a>
               </div>
-              <p className="title">{blog.title}</p>
             </div>
           );
         })}
